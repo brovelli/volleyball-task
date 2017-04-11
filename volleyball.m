320b6a499fef07565261576ab78eb3a27c3927d9
Andrea Brovelli <andrea.brovelli@univ-amu.fr>
2017-04-11 09:14:47 +0200

    Update volleyball.m

--- volleyball.m
+++ volleyball.m
@@ -183,26 +183,26 @@ for x = 1:nblocks
         
 % ========= END-OF-BLOCK QUESTIONS ========= %
         
-        DrawFormattedText(win,'Giudica l impatto di questo giocatore con un valore compreso tra -100 e +100.',150,300,white);
+        DrawFormattedText(win,'Giudica il valore di questo giocatore con un valore compreso tra -10 e +10.',150,300,white);
         DrawFormattedText(win,'Per esempio:',150,350,white);
-        DrawFormattedText(win,'   -100 : «questo giocatore fa sempre perdere la squadra»',150,450,white);
-        DrawFormattedText(win,'     0  : «questo giocatore non ha alcun impatto sulla performance della squadra»',150,500,white);
-        DrawFormattedText(win,'   +100 : «questo giocatore fa sempre vincere la squadra»',150,550,white);
+        DrawFormattedText(win,'   -10 : «Il giocatore fa sempre vincere la formazione in cui gioca»',150,450,white);
+        DrawFormattedText(win,'     0 : «Il giocatore non ha alcun impatto nella formazione»',150,500,white);
+        DrawFormattedText(win,'   +10 : «Il giocatore fa sempre perdere la formazione in cui gioca»',150,550,white);
         respQ1=Ask(win,'Inserisci il valore usando la tastiera, poi premi INVIO per continuare:   ',white,black,'GetChar',[800 300 1000 1000],'center',20);
         Screen('Flip',win);
         
-        DrawFormattedText(win,'Ora per favore indica con un valore compreso tra 0 e 100 quanto sicuro sei dell impatto del giocatore',150,300,white);
+        DrawFormattedText(win,'Ora indica quanto sicuro del valore assegnato al giocatore con un valore compreso tra 0 e 10 ',150,300,white);
         DrawFormattedText(win,'Per esempio:',150,350,white);
-        DrawFormattedText(win,'    0  : «per niente»',150,450,white);
-        DrawFormattedText(win,'   100 : «completamente»',150,500,white);
+        DrawFormattedText(win,'    0 : «per niente»',150,450,white);
+        DrawFormattedText(win,'   10 : «completamente»',150,500,white);
         respQ2=Ask(win,'Inserisci il valore usando la tastiera, poi premi INVIO per continuare:   ',white,black,'GetChar',[800 300 1000 1000],'center',20);
         Screen('Flip',win);
         
-        respQ3=Ask(win,'Compreresti questo giocatore per il prossimo campionato [si o no]?   ',white,black,'GetChar',[800 100 1000 1000],'center',20);
+        respQ3=Ask(win,'Compreresti questo giocatore per il prossimo campionato (si o no)?   ',white,black,'GetChar',[800 100 1000 1000],'center',20);
         Screen('Flip',win);
         
         DrawFormattedText(win,'Ne sei sicuro?',150,300,white);
-        respQ4=Ask(win,'0 = «per niente», 100 = «completamente»:   ',white,black,'GetChar',[800 100 1000 1000],'center',20);
+        respQ4=Ask(win,'0 : «per niente», 10 : «completamente»:   ',white,black,'GetChar',[800 100 1000 1000],'center',20);
         Screen('Flip',win);
         
         %Store responses after each block
